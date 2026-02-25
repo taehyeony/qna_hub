3b4e0e4ea5858d831c86d066113aa232cb69ea68
taehyeonJeon <taehyun0540@gmail.com>
2026-02-25 19:25:42 +0900

    feat: 회원가입 API 구현 - 이메일/닉네임 중복 체크 및 탈퇴 회원 복구 유도 - BCrpyt를 활용한 비밀번호 암호화 - API 공통 규격 및 DTO 설계 - 가입 실패 시나리오 단위 테스트 완료

--- src/main/java/com/taehyeon/qna/controller/UserController.java
+++ src/main/java/com/taehyeon/qna/controller/UserController.java
@@ -3,7 +3,6 @@ package com.taehyeon.qna.controller;
 import com.taehyeon.qna.dto.request.SignUpRequestDto;
 import com.taehyeon.qna.dto.response.ApiResponse;
 import com.taehyeon.qna.service.UserService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,11 +19,8 @@ public class UserController {
 
     @PostMapping("/signup")
     public ResponseEntity<ApiResponse<Void>> signUp(@RequestBody SignUpRequestDto signUpRequestDto){
-        System.out.println("컨트롤러 진입 확인");
         userService.signUp(signUpRequestDto);
-
         ApiResponse<Void> response = ApiResponse.success(null);
-        System.out.println("응답 객체 확인: " + response.getMessage()); // 콘솔에 찍히는지 확인
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
