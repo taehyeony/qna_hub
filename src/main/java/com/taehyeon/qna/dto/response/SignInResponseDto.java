349ab3a7d8880601d7e3da74469b6b4e74b3fdfa
taehyeonJeon <taehyun0540@gmail.com>
2026-03-03 10:04:17 +0900

    feat: 로그인 API 구현 - 이메일/비밀번호 검증 로직 추가 - AccessToken 및 RefreshToken 발급 - 유효하지 않은 자격 증명 시 공통 에러 처리

---
+++ src/main/java/com/taehyeon/qna/dto/response/SignInResponseDto.java
@@ -0,0 +1,15 @@
+package com.taehyeon.qna.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class SignInResponseDto {
+    private final String accessToken;
+}
