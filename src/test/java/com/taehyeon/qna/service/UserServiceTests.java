760fa567a0a82a3ec4552145fd9e38e37432d4aa
taehyeonJeon <taehyun0540@gmail.com>
2026-03-09 01:46:08 +0900

    chore: 테스트 DB 분리

--- src/test/java/com/taehyeon/qna/service/UserServiceTests.java
+++ src/test/java/com/taehyeon/qna/service/UserServiceTests.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.transaction.annotation.Transactional;
 
